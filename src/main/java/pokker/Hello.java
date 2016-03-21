1bd2365b109a72890ec6046742687cb9b645d0f0
henry300 <henry.teigar@gmail.com>
2016-03-21 13:36:11 +0200

    Maven structure

--- src/main/java/pokker/Hello.java
+++ src/main/java/pokker/Hello.java
@@ -1,5 +1,7 @@
+package pokker;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("This is a test class");
+        System.out.println("Hello");
     }
 }
