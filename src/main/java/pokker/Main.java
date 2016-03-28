a32458135128f84dd7566283caf95b7bac94930e
henry300 <henry.teigar@gmail.com>
2016-03-28 06:26:02 +0300

    Add necessary code to main to start the game

--- src/main/java/pokker/Main.java
+++ src/main/java/pokker/Main.java
@@ -1,8 +1,55 @@
 package pokker;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Player player1 = new Player("Madis");
-        player1.act(222);
+        // Lots of hardcoded stuff only for demo console version.
+
+        // Hardcoded values for demo only
+        int tableSize = 9;
+        int bigBlind = 20;
+        int startingMoney = 200;
+
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        Table table = new Table(tableSize, bigBlind);
+
+        // Hardcode test players
+        List<Player> players = new ArrayList<>();
+        players.add(new Player("Toomas"));
+        players.add(new Player("Madis"));
+        players.add(new Player("Kaspar"));
+        players.add(new Player("Peeter"));
+        players.add(new Player("Kuido"));
+        players.add(new Player("Tarvo"));
+
+        // Play as Peeter
+        players.get(3).setReal();
+
+
+        // Add players to the table with starting money
+        for (Player player : players) {
+            table.playerJoin(player);
+            player.recieveMoney(startingMoney);
+        }
+
+        // Start the new game
+        table.gameStart();
+        System.out.println("New game started! There are " + players.size() + " players, each of whom have " + startingMoney + "€ \n" +
+                "-------------------------------------------------------------");
+
+
+
+
+
+
+
+
+
     }
 }
