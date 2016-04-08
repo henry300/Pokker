de1ac2fbdc9439de1a0723486f4a5180f42f7f75
Oskar Köök <oskarkook@maatriks.ee>
2016-04-08 12:47:48 +0300

    stuff

---
+++ src/main/java/pokker/server/Main.java
@@ -0,0 +1,14 @@
+package pokker.server;
+
+public class Main {
+    public static void main(String[] args) {
+        // mängu algseis (pot, mängijate seisund, panused)
+        // mängija liitus (raha)
+        // kaardid jagatud
+        // panuse panek
+        // kes võitis
+        // mängija lahkus (peab vahet tegema lahkumisel ja disconnectil?)
+
+        // puudu: timer, kicker, bigblind preflop peaks saama actida, kui keegi ei raisinud
+    }
+}
