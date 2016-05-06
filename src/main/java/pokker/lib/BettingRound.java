dbf6275d56834f1768a19f2cbbc6ef83eb6cfda8
Oskar Köök <oskarkook@maatriks.ee>
2016-05-06 16:02:55 +0300

    Remove ordinal() use from BettingRound enum

--- src/main/java/pokker/lib/BettingRound.java
+++ src/main/java/pokker/lib/BettingRound.java
@@ -4,12 +4,22 @@ package pokker.lib;
  * Represents betting rounds that happen during a poker game.
  */
 public enum BettingRound {
-    PREFLOP,
-    FLOP,
-    TURN,
-    RIVER;
+    RIVER(null),
+    TURN(RIVER),
+    FLOP(TURN),
+    PREFLOP(FLOP);
 
-    public BettingRound next() {
-        return values()[ordinal() + 1];
+    private final BettingRound nextBettingRound;
+
+    BettingRound(BettingRound nextBettingRound) {
+        this.nextBettingRound = nextBettingRound;
+    }
+
+    public BettingRound next() throws NoNextBettingRoundException {
+        if (nextBettingRound == null) {
+            throw new NoNextBettingRoundException();
+        }
+
+        return nextBettingRound;
     }
 }
---
+++ src/main/java/pokker/lib/NoNextBettingRoundException.java
@@ -0,0 +1,4 @@
+package pokker.lib;
+
+public class NoNextBettingRoundException extends RuntimeException {
+}
