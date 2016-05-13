addd2472c076a5b16fb4b5bd7dfc6de6ae5fee45
henry300 <henry.teigar@gmail.com>
2016-05-14 00:53:28 +0300

    Remove console version up until actual game start

--- src/main/java/pokker/client/game/Main.java
+++ src/main/java/pokker/client/game/Main.java
@@ -10,7 +10,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.*;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -18,7 +19,6 @@ import javafx.util.Duration;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 /**
  * This is the class that runs the game.
@@ -31,47 +31,7 @@ public class Main extends Application{
     StackPane gameBackgroundPane;
 
     public static void main(String[] args) throws IOException {
-        // Start gui
         launch(args);
-
-        // Prevent other code from running (temporary)
-        System.exit(0);
-
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter your name:");
-        String name = scanner.next();
-        Game game = new Game(name);
-        System.out.println("Connecting you to the server...");
-        // TODO: allow user to specify the address in the format of "ip:port"
-        game.connect("localhost", 1337);
-        System.out.println("Connected!");
-        game.updateTables();
-        System.out.println();
-        System.out.println("Tables:");
-
-
-
-
-        List<TableClient> tables = game.getTables();
-        for (int i = 0; i < tables.size(); i++) {
-            TableClient table = tables.get(i);
-            System.out.println("Table " + (i + 1) + ": " + table.getPlayers().size() + "/" + table.getTableSize() + " players. " +
-                    "Big blind: " + table.getBigBlind());
-        }
-
-        System.out.println("Choose a table: ");
-        int tableNum = scanner.nextInt();
-        while (!game.joinTable(tables.get(tableNum - 1).getId())) {
-            System.out.println("The table is already full. Choose again.");
-            tableNum = scanner.nextInt();
-        }
-
-        System.out.println("You have successfully joined the table nr " + tableNum);
-
-        // TODO: fixme
-        // GSON problem: non-exposed fields are not initialized, which makes the following line throw an error.
-        // tables.get(tableNum - 1).listen(new TableShouter());
     }
 
     @Override
@@ -245,5 +205,9 @@ public class Main extends Application{
         gameBackgroundPane = new StackPane();
         gameBackgroundPane.getStylesheets().addAll("styles/styles.css", "styles/tableStyles.css");
         gameBackgroundPane.getStyleClass().add("background");
+    }
+
+    public void addSeats() {
+
     }
 }
