8a0c5b24159afadf44e7860d88da304fd5b0f621
henry300 <henry.teigar@gmail.com>
2016-05-17 01:07:47 +0300

    Add enter event handler to the INTRO view

--- src/main/java/pokker/client/gui/Gui.java
+++ src/main/java/pokker/client/gui/Gui.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -79,6 +80,17 @@ public class Gui extends Application{
             if (!name.equals("")) {
                 menuBackgroundPane.getChildren().remove(questionBox);
                 connectAndCreateNewGame(name);
+            }
+        });
+        
+        textField.setOnKeyPressed(e -> {
+            if (e.getCode().equals(KeyCode.ENTER))
+            {
+                String name = textField.getText();
+                if (!name.equals("")) {
+                    menuBackgroundPane.getChildren().remove(questionBox);
+                    connectAndCreateNewGame(name);
+                }
             }
         });
 
